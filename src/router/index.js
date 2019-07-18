e28f8218ddd26a101325eff1eab89f7b2f6b5d23
meng_zhao <meng_zhao@htxa.com.cn>
2019-07-18 16:56:32 +0800

    Initial commit

---
+++ src/router/index.js
@@ -0,0 +1,211 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+
+import Login from '@/views/login'
+import Layout from '@/layout/layout'
+
+Vue.use(Router)
+
+/**
+ * alwaysShow: true           if set true, will always show the root menu, whatever its child routes length
+ *                            if not set alwaysShow, only more than ont route under the children
+ *                            it will becomes nested mode, otherwise not show the root menu
+ * alwaysShow: true           如果设置为true,它将总是现在在根目录。如果不设置的话，当它只有1个子路由的时候，会把
+ *                            它的唯一子路由放到跟目录上来，而不显示它自己本身。
+ */
+
+export const constantRouterMap = [
+  {
+    path: '/login',
+    name: 'login',
+    hidden: true,
+    component: Login,
+    meta: {
+      title: '登录'
+    }
+  },
+  {
+    path: '/',
+    hidden: true,
+    component: Layout,
+    redirect: '/home',
+    children: [
+      {
+        path: 'home',
+        name: 'home',
+        component: () => import('@/views/homepage'),
+        meta: {title: '首页'}
+      }
+    ]
+  },
+  {
+    path: '/user',
+    component: Layout,
+    hidden: true,
+    meta: {
+      icon: 'tickets',
+      title: '个人中心'
+    },
+    children: [
+      {
+        path: 'profile',
+        name: 'Profile',
+        component: () => import('@/views/user/profile'),
+        meta: {icon: 'warning', title: '个人中心'}
+      },
+      {
+        path: 'avatar',
+        name: 'Avatar',
+        component: () => import('@/views/user/profile'),
+        meta: {icon: 'warning', title: '修改头像'}
+      }
+    ]
+  },
+  {
+    path: '/introduction',
+    component: Layout,
+    redirect: '/introduction/index',
+    // alwaysShow: true,
+    meta: {
+      icon: 'svg-aperture',
+      title: '简述'
+    },
+    children: [
+      {
+        path: 'index',
+        name: 'Introduction',
+        component: () => import('@/views/introduction/index'),
+        meta: {icon: 'svg-aperture', title: '简述'}
+      }
+    ]
+  },
+  {
+    path: '/nested',
+    component: Layout,
+    meta: {
+      icon: 'svg-layers',
+      title: '路由嵌套'
+    },
+    children: [
+      {
+        path: 'menu1',
+        meta: { icon: 'share', title: '嵌套路由1' },
+        component: () => import('@/views/nested/menu1/index'),
+        children: [
+          {
+            path: 'menu1-1',
+            name: 'menu1-1',
+            component: () => import('@/views/introduction/index'),
+            meta: {icon: 'success', title: '嵌套路由1-1'}
+          },
+          {
+            path: 'menu1-2',
+            name: 'menu1-2',
+            component: () => import('@/views/nested/menu1/menu1-2/index'),
+            alwaysShow: true,
+            meta: {icon: 'error', title: '嵌套路由1-2'},
+            children: [
+              {
+                path: 'menu1-2-1',
+                component: () => import('@/views/introduction/index'),
+                name: 'menu1-2-1',
+                meta: {icon: 'warning', title: '嵌套路由1-2-1'}
+              }
+            ]
+          }
+        ]
+      },
+      {
+        path: 'menu2',
+        name: 'menu2',
+        component: () => import('@/views/introduction/index'),
+        meta: {icon: 'star-on', title: '嵌套路由2'}
+      }
+    ]
+  },
+  {
+    path: '/custom-component',
+    component: Layout,
+    meta: {
+      icon: 'question',
+      title: '自定义组件'
+    },
+    redirect: '/custom-component/index',
+    children: [
+      {
+        path: 'index',
+        name: 'CustomComponent',
+        component: () => import('@/views/custom-component/index'),
+        meta: {icon: 'warning', title: '自定义组件'}
+      }
+    ]
+  },
+  {
+    path: '/echarts',
+    component: Layout,
+    redirect: 'index',
+    alwaysShow: true,
+    meta: {
+      title: '可视化',
+      icon: 'svg-droplet'
+    },
+    children: [
+      {
+        path: 'index',
+        name: '数量统计',
+        component: () => import('@/views/echarts'),
+        meta: {title: '数量统计', icon: 'svg-heart'}
+      }
+    ]
+  }
+]
+
+export default new Router({
+  // mode: 'history',  require service support
+  // scrollBehavior: () => ({ y: 0 }),
+  routes: constantRouterMap
+})
+
+export const asyncRouterMap = [
+  {
+    path: '/excel',
+    component: Layout,
+    redirect: '/excel/export-excel',
+    alwaysShow: true,
+    meta: {
+      title: '表格',
+      icon: 'edit'
+    },
+    children: [
+      {
+        path: 'export-excel',
+        name: '导出表格',
+        component: () => import('@/views/excel/export-excel'),
+        meta: {icon: 'arrow-left', title: 'exportExcel'}
+      },
+      {
+        path: 'upload-excel',
+        name: '上传表格',
+        component: () => import('@/views/excel/upload-excel'),
+        meta: {icon: 'minus', title: 'uploadExcel'}
+      }
+    ]
+  },
+  {
+    path: '/clipboard',
+    component: Layout,
+    redirect: 'index',
+    meta: {
+      icon: 'document',
+      title: '剪切板'
+    },
+    children: [
+      {
+        path: 'index',
+        name: 'clipBoard',
+        component: () => import('@/views/clipboard'),
+        meta: {title: '剪切板示例', icon: 'document'}
+      }
+    ]
+  }
+]
